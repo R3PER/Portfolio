46d59ca0569109216413bc3891acdd74e6639581
R3PER <gamestate1255@gmail.com>
2025-08-26 17:40:48 +0200

    fix urls

--- utils/paths.js
+++ utils/paths.js
@@ -1,14 +1,7 @@
-// Tymczasowo włączamy tryb debugowania, żeby zobaczyć faktycznie generowane ścieżki
 export const getAssetPath = (path) => {
   const cleanPath = path.startsWith('/') ? path.substring(1) : path;
   
-  // W wersji produkcyjnej dodajemy '/public/' przed ścieżką, ponieważ obrazy 
-  // znajdują się w folderze public w GitHub Pages
-  if (process.env.NODE_ENV === 'production') {
-    // Upewniamy się, że plik się załaduje z właściwej lokalizacji
-    return `/Portfolio/public/${cleanPath}`;
-  } else {
-    // W środowisku deweloperskim
-    return `/${cleanPath}`;
-  }
+  // W środowisku produkcyjnym ścieżki mają już prefiks /Portfolio dodawany przez Next.js
+  // Nie dodawajemy tu żadnych dodatkowych prefiksów, żeby uniknąć duplikacji
+  return cleanPath;
 };
