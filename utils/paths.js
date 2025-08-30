f283d41a13c3c38ac2f234d8814b07292ec04fe1
R3PER <gamestate1255@gmail.com>
2025-08-30 02:07:07 +0200

    fix path

--- utils/paths.js
+++ utils/paths.js
@@ -2,11 +2,7 @@ export const getAssetPath = (path) => {
   // Usuwamy wszelkie ścieżki początkowe, aby zapewnić, że używamy tylko czystej ścieżki do pliku
   const cleanPath = path.replace(/^\//, '');
   
-  // W środowisku produkcyjnym dodajemy explicite ścieżkę do folderu public, 
-  // ponieważ Next.js nie wie, że zasoby są w katalogu public
-  if (process.env.NODE_ENV === 'production') {
-    return `/Portfolio/public/${cleanPath}`;
-  } else {
-    return `/${cleanPath}`;
-  }
+  // W Next.js, pliki w folderze /public są serwowane bezpośrednio z katalogu głównego
+  // Nie potrzebujemy dodawać prefiksu /Portfolio/public/ w środowisku produkcyjnym
+  return `/${cleanPath}`;
 };
